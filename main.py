a82fad9af9c65ad774e97f54f7d27b4059330d9e
Kumbrle <86300730+Kumbrle@users.noreply.github.com>
2023-06-25 14:00:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import request
+import requests
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+@app.route("/", methods = ["Get"]) 
+def home():
+  url = request.args.get("vtt")
+  r = requests.get(url, allow_redirects=True)
+  r.headers.add("Access-Control-Allow-Origin", "*")
+  return r.content
 
 if __name__ == '__main__':
   app.run(port=5000)
